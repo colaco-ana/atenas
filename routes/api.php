c600542c6a8c53d4d0c9be14d27c6d2c9fe7606d
Luisaamj <luisaamj@ua.pt>
2020-01-10 13:04:00 +0000

    Versão Autenticacao Luisa

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::resource('author','AuthorController');
+
 Route::resource('authorUser','AuthorUserController');
 Route::resource('category','CategoryController');
 Route::resource('categoryPublication','CategoryPublicationController');
@@ -26,6 +26,9 @@ Route::resource('publication','PublicationController');
 Route::resource('search','SearchController');
 Route::resource('searchType','SearchTypeController');
 Route::resource('topic','TopicController');
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::resource('author','AuthorController');
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
